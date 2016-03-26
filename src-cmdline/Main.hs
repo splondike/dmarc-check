e1cae8e5685b45b916ace0c9f7b3d041b6f11a3e
Stefan Schneider-Kennedy <code@stefansk.name>
2016-03-26 11:33:28 +1100

    Replace commandline args processing code

--- src-cmdline/Main.hs
+++ src-cmdline/Main.hs
@@ -1,32 +1,24 @@
-import System.Environment (getArgs)
-import Data.Maybe (fromMaybe)
-import qualified System.Console.GetOpt as GO
+import Data.Monoid ((<>))
+import Control.Applicative (pure, (<*>))
+
+import Options.Applicative
 
 import qualified DmarcCheck as DmarcCheck
 
-main = do
-   opts <- getOpts
-   DmarcCheck.program (configPath opts) (includeHeader opts)
-
-getOpts = do
-   rawArgs <- getArgs
-   case GO.getOpt GO.Permute options rawArgs of
-      (o,n,[]  ) -> return $ foldl (flip id) defaultOpts o
-      (_,_,errs) -> ioError (userError (concat errs ++ usage))
+main = execParser progOpts >>= \opts ->
+   DmarcCheck.program (configPath opts) (not $ hideHeader opts)
 
 data Opts = Opts {
    configPath :: String,
-   includeHeader :: Bool
+   hideHeader :: Bool
 } deriving (Show)
 
-defaultOpts = Opts {
-   configPath = "dmarc-check.conf",
-   includeHeader = True
-}
-
-options = [config, includeHeader]
+progOpts = info (helper <*> opts) fullDesc
    where
-      config = GO.Option "c" ["config"] (GO.ReqArg (\val opts -> opts { configPath = val}) "CONFIG") "The configuration file containing IMAP connection information"
-      includeHeader = GO.Option "" ["hideHeaders"] (GO.NoArg (\opts -> opts {includeHeader = False})) "Suppress the CSV headers"
-
-usage = GO.usageInfo "Usage: dmarc-check --config <dmarc-check.conf> --hideHeaders\n\nProduce a CSV report based on the mailbox referenced in dmarc-check.conf" options
+      opts = pure Opts 
+         <*> strOption (long "config"
+                     <> short 'c'
+                     <> metavar "CONFIG"
+                     <> help "The configuration file containing IMAP connection information")
+         <*> switch    (long "hideHeader"
+                     <> help "Suppress the CSV header line")
